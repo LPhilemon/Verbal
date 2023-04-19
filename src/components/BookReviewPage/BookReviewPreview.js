6766116f2c14a086109be0a08d734deb818c0710
lphilemon <lphilemon23@gmail.com>
2023-04-19 09:26:53 +0300

    [HomeDisplay DataFetch]

---
+++ src/components/BookReviewPage/BookReviewPreview.js
@@ -0,0 +1,46 @@
+import ArticleListItem from "../articles/ArticleListItem";
+import NoImageArticleListItem from "../Articles/NoImageArticleListItem";
+import Link from "next/link";
+import styles from "../../styles/ArticleListItem.module.css";
+
+const BookReviewPreview = ({ article }) => {
+    const { title, author, publishedAt, excerpt, slug, imageURL } = article;
+    return (
+      <li>
+        <div class="o-card o-card_stretch">
+          <Link href={`/articles/${slug}`}>
+            {imageURL && (
+              <img
+                className={styles["img-responsive"]}
+                src={imageURL}
+                alt={title}
+              />
+            )}
+          </Link>
+          <div class="o-card-bd o-card-bd_constrained">
+            <div class="c-feature">
+              <div class="c-feature-preamble">
+                <span class="c-txt c-txt_catMeta">Book Review</span>
+              </div>
+  
+              <div class="c-feature-hd">
+                <h2 class="c-hdgSans c-hdgSans_2">
+                  <Link href={`/articles/${slug}`}>{title}</Link>
+                </h2>
+              </div>
+  
+              <div class="c-feature-sub c-feature-sub_flat">
+                <span class="c-txt c-txt_attribution">By {author}</span>
+              </div>
+  
+              <div class="c-feature-sub">
+                <span class="c-txt c-txt_attribution">Reviewed By {author}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+  };
+  
+  export default BookReviewPreview;
---
+++ src/components/BookReviewPage/BookReviewSamples.js
@@ -0,0 +1,35 @@
+import ArticleListItem from "../articles/ArticleListItem";
+import NoImageArticleListItem from "../Articles/NoImageArticleListItem";
+import Link from "next/link";
+import styles from "../../styles/ArticleListItem.module.css";
+import BookReviewPreview from "./BookReviewPreview";
+
+const BookReviewSamples = ({ articles }) => {
+  
+
+    return (
+        <div class="c-tier c-tier_weighted c-mix-tier_offsetDistant">
+          <div class="c-tier-label">
+            <span class="c-txt c-txt_herald">
+              <p>Latest Book Reviews</p>
+            </span>
+          </div>
+    
+          <div class="c-tier-content">
+            <ul class="noPadding-noListStyle o-sectionList">
+              <li>
+                <ul class="noPadding-noListStyle o-graphBlocks o-graphBlocks_2up@xsmall o-graphBlocks_3up@medium">
+                  {articles.map((article, index) => (
+                    <BookReviewPreview key={index} article={article} />
+                  ))}
+                </ul>
+              </li>
+            </ul>
+            {/* <!-- end .sectionList --> */}
+          </div>
+        </div>
+      );
+    };
+    
+    export default BookReviewSamples;
+    
