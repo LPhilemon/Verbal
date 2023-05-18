ab0e2b56648381694b290a8fd5b5dac7e0cec530
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-18 17:33:28 +0300

    [Added CLientApp.js to github instead of adding entire file to git ignore Api key added to .env file safe to publically commit]

---
+++ firebase/clientApp.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+//import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+//const analytics = analytics.isSupported(getAnalytics(app));
+
+export { db, auth, storage };//analytics 
+
