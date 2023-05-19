1051d136b492c89e2748f7afe380cd5d821d7b2c
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-19 16:43:46 +0300

    [Added changes to env variables for production
    appending NEXT_PUBLIC]

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -5,17 +5,20 @@ import { getStorage } from "firebase/storage";
 //import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  
 };
+// const apiKey = firebaseConfig.apiKey; // Assign the apiKey value to a variable
+// console.log("API Key:", apiKey); // Log the apiKey value
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log("Firebase app initialized"); 
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
